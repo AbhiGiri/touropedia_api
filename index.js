51e4ee341ae9e954f2cb84c51f8f7eddbe653ad1
AbhiGiri <abhishekmkumar003@gmali.com>
2023-04-01 22:52:26 +0530

    first

--- index.js
+++ index.js
@@ -22,10 +22,8 @@ app.use('/', (req, res) => {
 
 
 const port = process.env.PORT || 5000;
-mongoose
-  .connect(process.env.MONGODB_URI)
-  .then(() => {
-    app.listen(port, () => console.log(`Server running on port ${port}`));
-  })
+
+mongoose.connect(process.env.MONGODB_URI)
+  .then(() => app.listen(port, () => console.log(`Server running on port ${port}`)))
   .catch((error) => console.log(`${error} did not connect`));
 
